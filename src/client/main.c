19e4cd52ff1a628f066d3dc648f377c3c5610eda
dmaivel <dvmaivel@gmail.com>
2023-08-04 15:09:07 -0400

    Added GLX implementation, changed linux lib name

--- src/client/main.c
+++ src/client/main.c
@@ -4,13 +4,6 @@
 
 void __attribute__((constructor)) sharedgl_entry(void) 
 {
-    /*
-     * platform dependent hooks
-     */
-    hook_install("glXSwapBuffers", glXSwapBuffers);
-    // hook_install("glXGetProcAddressARB", glXGetProcAddressARB);
-    // hook_install("glXGetProcAddress", glXGetProcAddress);
-
     glimpl_init();
 }
 
