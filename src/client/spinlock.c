122b4243e47ef71f63a744792ca53760b8c04365
dmaivel <dvmaivel@gmail.com>
2023-09-02 18:26:47 -0400

    Add Windows equivalent for spinlock

--- src/client/spinlock.c
+++ src/client/spinlock.c
@@ -1,5 +1,9 @@
 #include <client/spinlock.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 static int *keep_this_lock_pls;
 
 void spin_set(int *lock)
@@ -14,11 +18,17 @@ int *spin_get()
 
 void spin_lock(int *lock)
 {
+#ifndef _WIN32
     while (!__sync_bool_compare_and_swap(lock, 0, 1));
+#else
+    while (!InterlockedCompareExchange(lock, 1, 0));
+#endif
 }
 
 void spin_unlock(int volatile *lock)
 {
+#ifndef _WIN32
     asm volatile ("":::"memory");
+#endif
     *lock = 0;
 }
