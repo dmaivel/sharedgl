baf257626dee8bfaba03b8044c6378dbc17c24e0
dmaivel <maiveldavid@gmail.com>
2023-09-03 17:30:50 -0400

    Add `_mm_pause` to spinlock

--- src/client/spinlock.c
+++ src/client/spinlock.c
@@ -1,4 +1,5 @@
 #include <client/spinlock.h>
+#include <emmintrin.h>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -19,9 +20,11 @@ int *spin_get()
 void spin_lock(int *lock)
 {
 #ifndef _WIN32
-    while (!__sync_bool_compare_and_swap(lock, 0, 1));
+    while (!__sync_bool_compare_and_swap(lock, 0, 1))
+        _mm_pause();
 #else
-    while (!InterlockedCompareExchange(lock, 1, 0));
+    while (!InterlockedCompareExchange(lock, 1, 0))
+        _mm_pause();
 #endif
 }
 
